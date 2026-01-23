1ac87813c4e005cdd471fc42cb05dcf39e0d7d4e
Lilyfc <coralcatcodes@gmail.com>
2026-01-23 15:11:08 -0700

    Part 2.4: courseinfo - map to scale N courses, each with M parts

--- part2/courseinfo2/src/App.jsx
+++ part2/courseinfo2/src/App.jsx
@@ -1,91 +1,154 @@
-// const Header = (props) => <h1>{props.name}</h1>
-
-const Header = (props) => {
-  // console.log('Header props:', props)
-  return <h1>{props.name}</h1>
-}
-
-const Content = (props) => {
-  // console.log('Content props:', props)
-  // console.log('slice', props.part[0])
-    return (
-      <>
-    <div>
-      <Part part={props.part[0]} />
-      <Part part={props.part[1]} />
-      <Part part={props.part[2]} />
-    </div>
-    </>
-    )
-}
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
+// console logging CourseContent before mapping it
+// const CourseContent = (props) => {
+//   console.log('course content props:', props)
+//   return (
+//     <p>
+//       <p>{JSON.stringify(props.parts[0])}</p>
+//     </p>
+//   )
+// }
 
 const Total = (props) => {
-  //const first = props.part[0].exercises
-  // console.log('Total props:', props.total[0].exercises)
-  // old manual method
-  // const a = props.total[0].exercises
-  // const b = props.total[1].exercises
-  // const c = props.total[2].exercises
-  // const total = a+b+c
-
-  const total = props.total.reduce(function(sum, part){
+  // console.log('All total props', props)
+  // console.log('Total props', props.total)
+  const total = props.parts.reduce(function(sum, part){
     return sum + part.exercises
-  }, 0)
+  },0)
+  // console.log('total constant', total)
 
   return (
-    <h4>Total of {total} exercises</h4>
+    <h4>total of {total} exercises</h4>
   )
+}
+
+const Part = (props) => {
+  //console.log('Part props:', props)
+  return (
+    <>
+      <p>{props.name} {props.exercises}</p>
+    </>
+  )
+}
+
+const CourseContent = (props) => {
+  // console.log('course content props:', props)
+  return (
+    <>
+      {props.parts.map(function(part){
+          // console.log('map part.id', part.id)
+          // console.log('map part.name',part.name)
+          // console.log('map part.exercises',part.exercises)
+          return (
+            <Part 
+              key={part.id}
+              name={part.name}
+              exercises={part.exercises}
+            />
+          )
+        })
+      }
+    </>
+  )
+}
+
+const CourseName = (props) => {
+  //console.log('Header props:', props)
+  return <h2>{props.name}</h2>
 }
 
 const Course = (props) => {
-  //console.log('course:',props.course)
-  //console.log(props.course.id)
-  //console.log(props.course.parts)
+  //console.log('course props:', props)
   return (
     <>
-      <Header 
-        name={props.course.name}
+      <CourseName
+        name={props.name} 
       />
-      <Content 
-        part={props.course.parts}
+      <CourseContent 
+        parts={props.parts}
       />
       <Total 
-        total={props.course.parts}
+        parts={props.parts}
       />
     </>
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+const Courses = (props) => {
+  //console.log('courses:',props.courses)
+  //console.log('first course parts', props.courses[0].parts)
+  return (
+    <>
+      {props.courses.map(function(course){
+          // console.log('course.id:',course.id)
+          // console.log('course.name:',course.name)
+          // console.log('course.parts',course.parts)
+          return (
+            <Course 
+              key={course.id}
+              name={course.name}
+              parts={course.parts}
+            />
+          )
+        }
+      )}
+    </>
+  )
+}
 
-  return <Course course={course} />
+const Header = () => <h1>Web Development Curriculum</h1>
+
+const App = () => {
+const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
+      <Header />
+      <Courses courses={courses} />
+    </>
+  )
 }
 
 export default App
