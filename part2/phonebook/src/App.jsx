aa06bef81af964ea6d49358a1ce8731cd119f2e2
Lilyfc <coralcatcodes@gmail.com>
2026-01-24 10:53:44 -0700

    Part 2.11: phonebook - db.json, json-server, axios with useEffect mounting

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import Filter from '../components/Filter'
 import PersonForm from '../components/PersonForm'
 import Persons from '../components/Persons'
+import axios from 'axios'
+
+// const promise = axios.get('http://localhost:3001/persons')
+// console.log('axios promise:', promise)
+
+// promise.then(function(response){
+//   //console.log('promise response', response.data)
+//   return response.data
+// })
 
 const App = function () {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(function(){
+    //console.log('effect')
+    axios.get('http://localhost:3001/persons')
+      .then(function(response){
+        //console.log('promise fulfilled')
+        // console.log('promise response', response)
+        setPersons(response.data)
+      })
+  }, [])
+  //console.log('render',persons.length, 'persons')
 
   const handleFilterChange = function (event) {
     setFilter(event.target.value)
