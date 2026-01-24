b9eaa49bb21e351e255a0672acf3e6a74cb3865d
Lilyfc <coralcatcodes@gmail.com>
2026-01-23 20:13:25 -0700

    Part 2.9: phonebook - bonus - add search filter without case sensitivity

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,10 +3,23 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState(
     [
-    { name: 'Arto Hellas', number: '39-44-5323523' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleFilterChange = function(event){
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter(function(person){
+    return person.name.toLowerCase().includes(filter.toLowerCase())
+  })
 
   const handleAddName = function(event){
     event.preventDefault()
@@ -26,6 +39,7 @@ const App = () => {
       name: newName,
       number: newNumber
     }
+    
     setPersons(persons.concat(nameObject))
     setNewName('')
     setNewNumber('')
@@ -42,6 +56,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={filter} onChange={handleFilterChange}/> </div>
       <form onSubmit={handleAddName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -53,9 +68,10 @@ const App = () => {
         <br />        
          <div>debugName: {newName}</div>
          <div>debugNumber: {newNumber}</div>
+         <div>debugFilter: {filter}</div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(function(person){
+      {personsToShow.map(function(person){
           return (<div key={person.name}>{person.name} {person.number}</div>)
       })}
     </div>
