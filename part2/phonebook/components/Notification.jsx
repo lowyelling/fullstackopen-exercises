5f5f09a5df17fd2b282464115f1ee717b06a7bb0
Lilyfc <coralcatcodes@gmail.com>
2026-01-25 13:50:55 -0700

    Part 2.17: fixed Notification conditional render - didn't stage or commit on part2-2.17 branch

--- part2/phonebook/components/Notification.jsx
+++ part2/phonebook/components/Notification.jsx
@@ -1,19 +1,29 @@
-const Notification = ({ message }) => {
-  const notifStyle = {
-    color: 'green',
-    background: 'lightgrey',
-    fontSize: '20px',
-    borderStyle: 'solid',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '10px'
+const Notification = ({ message, type }) => {
+  if (message === null ){
+    return null
   }
 
-  return (
-    <div style={notifStyle}>
-        {message}
-    </div>
-  )
+const baseStyle = {
+  color: 'green',
+  background: 'lightgrey',
+  fontSize: '20px',
+  borderStyle: 'solid',
+  borderRadius: '5px',
+  padding: '10px',
+  marginBottom: '10px'
+}
+
+const colorStyle = type === 'error'
+  ? { color: 'red'}
+  : { color: 'green'}
+
+const notifStyle = {...baseStyle, ...colorStyle}
+
+return (
+  <div style={notifStyle}>
+      {message}
+  </div>
+)
 }
 
 export default Notification
